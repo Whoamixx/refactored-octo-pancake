1ec8d38e3158665330093fc9d3970df5a312f68c
Whoamixx <1531494296@qq.com>
2019-04-25 22:09:09 +0800

    commits

--- homeworks/test/2-1.cpp
+++ homeworks/test/2-1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 class Student
@@ -11,16 +12,46 @@ class Student
 		static double allscore;
 		static double highscore;
 		static double lowscore;
+		static double p;
 	public:
-		void output();
-		Student(int number_,char *name_,double s);
+		Student(int number_,const char *name_,double score_);
+		void StudentShow();
+		static void Show();
 };
-void Student::output()
+int Student::allnumber=0;
+double Student::highscore=0;
+double Student::lowscore=101;
+double Student::allscore=0;
+double Student::p=0;
+int main()
+{
+	Student stu1(1,"jiachengdong",99);
+	stu1.StudentShow();
+	Student stu2(2,"shaofeichen",100);
+	stu2.StudentShow();
+	Student stu3(3,"hongyucao",80);
+	stu3.StudentShow();
+	Student::Show();
+	return 0;
+}
+
+void Student::StudentShow()
 {
 	cout<<"number:"<<number<<" name:"<<name<<" score:"<<score<<endl;
 }
-Student::Student(int number_,char *name_，double s)
+
+Student::Student(int number_,const char *name_,double score_)
 {
 	number=number_;
 	strcpy(name,name_);
-	score=s;
+	score=score_;
+	allnumber++;
+	allscore+=score_;
+	if(score_>highscore){highscore=score_;}
+	if(score_<lowscore){lowscore=score_;}
+}
+void Student::Show()
+{
+	p=allscore/allnumber;
+	cout<<"allnumber:"<<allnumber<<" allscore:"<<allscore<<" highscore:"<<highscore<<" lowscore:"<<lowscore<<" avenge:"<<p<<endl;
+}
