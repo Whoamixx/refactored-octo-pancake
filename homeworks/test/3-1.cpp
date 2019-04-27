e378db10f93d788b0863aba6261caca5907cce4e
Whoamixx <1531494296@qq.com>
2019-04-27 20:44:58 +0800

    commits

--- homeworks/test/3-1.cpp
+++ homeworks/test/3-1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#define PI 3.1415
 using namespace std;
 class Point
 {
@@ -6,22 +7,46 @@ class Point
 		int x;
 		int y;
 	public:
-		Point(int x=0,int y=0);
+		Point(int a,int b):x(a),y(b){cout<<"Point construction"<<endl;}
+		~Point(){cout<<"Point Deconstruction"<<endl;}
 };
 class Circle:public Point
 {
 	protected:
 		int radius;
+		double area;
 	public:
-		Circle(int x,int y,int r):Point(x,y),radius(r){}
+		Circle(int x,int y,int r):Point(x,y),radius(r){cout<<"Circle construction"<<endl;}
+		~Circle(){cout<<"Circle Deconstruction"<<endl;}
+		void Area();
 };
 class Cylinder:public Circle
 {
 	protected:
 		int high;
+		double volume;
 	public:
-		Cylinder(int x,int y,int r,int h):Circle(x,y,r),high(h){}
+		Cylinder(int x,int y,int r,int h):Circle(x,y,r),high(h){cout<<"Cylinder construction"<<endl;}
+		~Cylinder(){cout<<"Cylinder Deconstruction"<<endl;}
+		void Volume();
 };
+void Circle::Area()
+{
+	area=PI*radius*radius;
+	cout<<"The area is "<<area<<endl;
+}
+void Cylinder::Volume()
+{
+	volume=area*high;
+	cout<<"The volume is "<<volume<<endl;
+}
 int main()
 {
+	cout<<"Enter main"<<endl;
 	Cylinder c(0,0,2,2);
+	c.Area();
+	c.Volume();
+	cout<<"Out main"<<endl;
+	return 0;
+}
+
